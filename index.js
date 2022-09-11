5bddb931ab51ef8f585c2a294565d1964b973e48
Red Oral <redmunozoral@gmail.com>
2022-09-11 16:39:01 -0700

    Changes to code and display

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import Snoowrap from 'snoowrap';
 import * as dotenv from 'dotenv';
 
 /** Variables for filter */
+/**   ALL MUST BE LOWERCASE */
 /**   'flair' is REQUIRED (Buying/Selling/Trading/etc.) */
-/**   I do suggest putting in a country filter (US/EU/PH/etc.) */
-/**   Keywords A and B are optional, leave keywords blank string to not use look for specific keywords */
-const country = 'US';
-const flair = 'Selling';
-const keywordA = '';
-const keywordB = '';
+/**   I do suggest putting in a country filter (us/eu/ph/etc.) */
+/**   Keyword is optional, use when looking for a specific item (ex: 65) */
+const country = 'us';
+const flair = 'selling';
+const keyword = '65';
 
 /** Initialize dotenv */
 dotenv.config();
@@ -23,6 +23,14 @@ const client = new Snoowrap({
   password: process.env.REDDIT_PASS
 });
 
+/** Info display */
+console.log('\n***************************');
+console.log('Reddit Market Bot by Red');
+console.log('***************************');
+console.log('Country: ' + country);
+console.log('Flair: ' + flair);
+console.log('Keyword: ' + 65);
+
 /** Listens to new submissions every 30 seconds on r/mechmarket */
 const submissions = new SubmissionStream(client, {
   subreddit: 'mechmarket',
@@ -30,14 +38,20 @@ const submissions = new SubmissionStream(client, {
   pollTime: 30000
 });
 
-/** Logs submission titles based on the given filter variables above **/
+/** Logs submission titles based on the given filter variables **/
 submissions.on('item', (item) => {
-  if (
-    item.link_flair_text === flair &&
-    item.title.startsWith('[' + country) &&
-    (item.title.includes(keywordA) || item.title.includes(keywordB))
-  ) {
-    console.log('\n[' + item.link_flair_text + '] ' + item.title);
-    console.log(item.url);
+  const postTitle = item.title.toLowerCase();
+  const postFlair = item.link_flair_text.toLowerCase();
+
+  if (postFlair === flair && postTitle.startsWith('[' + country)) {
+    /** If keyword is not empty and the app finds the word on the post title,
+     *   text will turn to green and the link to cyan*/
+    if (postTitle.includes(keyword) && keyword) {
+      console.log('\x1b[32m', '\n[' + item.link_flair_text + '] ' + item.title);
+      console.log('\x1b[36m%s\x1b[0m', item.url);
+    } else {
+      console.log('\n[' + item.link_flair_text + '] ' + item.title);
+      console.log(item.url);
+    }
   }
 });
