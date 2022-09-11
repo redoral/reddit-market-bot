fa3934bc9cb995c512a5a799b18c52f3b92454f7
Red Oral <redmunozoral@gmail.com>
2022-09-11 13:35:22 -0700

    Added comments, increased pollTime to 30 seconds

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { SubmissionStream } from 'snoostorm';
 import Snoowrap from 'snoowrap';
 import * as dotenv from 'dotenv';
 
+/** Initialize dotenv */
 dotenv.config();
 
+/** Create snoowrap client using dotenv variables */
 const client = new Snoowrap({
   userAgent: 'reddit-market-bot',
   clientId: process.env.CLIENT_ID,
@@ -12,12 +14,15 @@ const client = new Snoowrap({
   password: process.env.REDDIT_PASS
 });
 
+/** Listens to new submissions every 30 seconds on r/mechmarket */
 const submissions = new SubmissionStream(client, {
   subreddit: 'mechmarket',
   limit: 10,
-  pollTime: 2000
+  pollTime: 30000
 });
 
+/** Logs submission titles that have 65 or 75 in the title to the console */
+/**   65 or 75 specifically for keyboards with the respective layouts */
 submissions.on('item', (item) => {
   if (item.title.includes('65') || item.title.includes('75')) {
     console.log('[' + item.link_flair_text + '] ' + item.title);
