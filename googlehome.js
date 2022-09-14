f88756af3a7eb46a2dc4c66d086e4abe55418e4f
Red Oral <redmunozoral@gmail.com>
2022-09-13 19:17:08 -0700

    Working Chromecast integration for search match notifications (needs refactoring)

---
+++ googlehome.js
@@ -0,0 +1,29 @@
+import ChromecastAPI from 'chromecast-api';
+import * as googleTTS from 'google-tts-api';
+
+const googleHomeNotify = (numOfItems, sub) => {
+  const msg =
+    numOfItems + `${numOfItems === 1 ? 'match' : 'matches'}` + ' have been found on ' + sub;
+
+  const url = googleTTS.getAudioUrl(msg, {
+    lang: 'en',
+    slow: false,
+    host: 'https://translate.google.com'
+  });
+
+  const client = new ChromecastAPI();
+
+  client.on('device', (device) => {
+    device.play(url, function (err) {
+      if (!err) console.log('\nPlaying notification on ' + device.friendlyName + '.');
+    });
+
+    device.on('finished', () => {
+      device.close((err) => {
+        if (!err) console.log('Disconnected from ' + device.friendlyName + '.');
+      });
+    });
+  });
+};
+
+export default googleHomeNotify;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { SubmissionStream } from 'snoostorm';
 import Snoowrap from 'snoowrap';
 import * as dotenv from 'dotenv';
+import googleHomeNotify from './googlehome.js';
 
 /** Variables for filter */
 /**   I do suggest putting in a country filter (US/EU/etc.) */
 /**   country and keyword are both optional, use keyword when looking for a specific item (ex: 65) */
-const country = 'US';
-const keyword = 'qk65';
+const sub = 'mechmarket';
+const country = '';
+const keyword = 'paypal';
 
 /** Initialize dotenv */
 dotenv.config();
@@ -24,28 +26,45 @@ const client = new Snoowrap({
 console.log('\n***************************');
 console.log('Reddit Market Bot by Red');
 console.log('***************************');
-console.log('Country: ' + country);
+console.log('Country: ' + (country ? country : 'N/A'));
 console.log('Keyword: ' + (keyword ? keyword : 'N/A'));
-console.log('Keyword Optional: ' + (keywordOpt ? keywordOpt : 'N/A'));
 console.log('***************************');
 
 /** Listens to new submissions every 30 seconds on r/mechmarket */
 const submissions = new SubmissionStream(client, {
-  subreddit: 'mechmarket',
+  subreddit: sub,
   limit: 10,
-  pollTime: 2000
+  pollTime: 5000
 });
+
+/** Variables to keep track of posts per poll */
+var matches = [];
+var postCount = 0;
+var initialPoll = true;
 
 /** Logs submission titles based on the given filter variables **/
 submissions.on('item', (item) => {
+  postCount++;
   const postTitle = item.title.toLowerCase();
 
-  if (
-    item.title.startsWith('[' + country.toUpperCase) &&
-    postTitle.includes(keyword.toLowerCase())
-  ) {
+  if (item.title.startsWith('[' + country) && postTitle.includes(keyword.toLowerCase())) {
     console.log('\x1b[32m', '\n[' + item.link_flair_text + '] ' + item.title);
     console.log('\x1b[36m%s\x1b[0m', item.url);
-    console.log('*MATCH: ' + keyword + '*');
+    matches.push(postTitle);
+  }
+
+  /** Google Home casting */
+  if (keyword && postCount === 10 && initialPoll == true) {
+    if (matches.length > 0) {
+      googleHomeNotify(matches.length.toString(), sub);
+    }
+    postCount = 0;
+    matches = [];
+    initialPoll = false;
+  } else if (keyword && initialPoll == false) {
+    if (matches.length > 0) {
+      googleHomeNotify(1, sub);
+      matches = [];
+    }
   }
 });
